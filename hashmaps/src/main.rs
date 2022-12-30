e78e37aed69eec18a487f74aaa2f330dee640e26
Mark Grealish <mark@bhalash.com>
2022-12-30 22:41:15 +0000

    add learning example for hashmaps

---
+++ hashmaps/src/main.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Puppy {
+    name: String,
+}
+
+fn main() {
+    let jonsey = Puppy { name: "Jonsey".to_string() };
+    let buddy = Puppy { name: "Buddy".to_string() };
+    let pearl = Puppy { name: "Pearl".to_string() };
+
+    let mut puppies = HashMap::new();
+
+    puppies.insert(&jonsey.name, &jonsey);
+    puppies.insert(&buddy.name, &buddy);
+    puppies.insert(&pearl.name, &pearl);
+
+    let kiki = Puppy { name: "Kiki".to_string() };
+    let kiki = puppies.entry(&kiki.name).or_insert(&kiki);
+
+    println!("{:?}", jonsey);
+    println!("{:?}", kiki);
+}
